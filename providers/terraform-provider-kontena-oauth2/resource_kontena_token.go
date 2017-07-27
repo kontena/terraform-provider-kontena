2cab11ebfd11d1cb51d5737e1e5c566fbc8273db
Tero Marttila <tero.marttila@kontena.io>
2017-07-27 14:08:29 +0300

    kontena-oauth2_token: compute user, email, roles

--- providers/terraform-provider-kontena-oauth2/resource_kontena_token.go
+++ providers/terraform-provider-kontena-oauth2/resource_kontena_token.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/kontena/kontena-client-go/api"
 	"github.com/kontena/kontena-client-go/client"
 )
 
@@ -20,6 +21,21 @@ func resourceKontenaToken() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"user": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"email": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"roles": &schema.Schema{
+				Type: schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Computed: true,
+			},
 		},
 
 		Create: resourceKontenaTokenCreate,
@@ -30,6 +46,47 @@ func resourceKontenaToken() *schema.Resource {
 
 func tokenID(token *client.Token) string {
 	return fmt.Sprintf("%s", token.AccessToken)
+}
+
+func resourceKontenaTokenSync(providerMeta *providerMeta, rd *schema.ResourceData) error {
+	var code = rd.Get("code").(string)
+	var token = rd.Get("token").(string)
+	var user api.User
+
+	// check token still exists
+	if clientToken, err := client.MakeToken(token); err != nil {
+		// XXX: corrupt, force re-exchange?
+		return err
+	} else if apiClient, err := providerMeta.connectClient(clientToken); err != nil {
+		return err
+	} else if getUser, err := apiClient.Users.GetUser(); err == nil {
+		log.Printf("[INFO] Kontena-OAuth2 Token %v: Read code=%v token=%v ok: %#v", rd.Id(), code, token, getUser)
+
+		user = getUser
+
+	} else if forbiddenError, ok := err.(client.ForbiddenError); ok {
+		log.Printf("[INFO] Kontena-OAuth2 Token %v: Read code=%v token=%v gone: %v", rd.Id(), code, token, forbiddenError)
+
+		rd.SetId("")
+
+		return nil
+
+	} else {
+		log.Printf("[INFO] Kontena-OAuth2 Token %v: Read code=%v token=%v err: %v", rd.Id(), code, token, err)
+
+		return err
+	}
+
+	rd.Set("user", user.Name)
+	rd.Set("email", user.Email)
+
+	var roles []string
+	for _, apiRole := range user.Roles {
+		roles = append(roles, apiRole.Name)
+	}
+	rd.Set("roles", roles)
+
+	return nil
 }
 
 func resourceKontenaTokenCreate(rd *schema.ResourceData, meta interface{}) error {
@@ -46,35 +103,14 @@ func resourceKontenaTokenCreate(rd *schema.ResourceData, meta interface{}) error
 		rd.SetId(tokenID(clientToken))
 	}
 
-	return nil
+	// sync
+	return resourceKontenaTokenSync(providerMeta, rd)
 }
 
 func resourceKontenaTokenRead(rd *schema.ResourceData, meta interface{}) error {
 	var providerMeta = meta.(*providerMeta)
 
-	var code = rd.Get("code").(string)
-	var token = rd.Get("token").(string)
-
-	// check token still exists
-	if clientToken, err := client.MakeToken(token); err != nil {
-		// XXX: corrupt, force re-exchange?
-		return err
-	} else if apiClient, err := providerMeta.connectClient(clientToken); err != nil {
-		return err
-	} else if user, err := apiClient.Users.GetUser(); err == nil {
-		log.Printf("[INFO] Kontena-OAuth2 Token %v: Read code=%v token=%v ok: %#v", rd.Id(), code, token, user)
-	} else if forbiddenError, ok := err.(client.ForbiddenError); ok {
-		log.Printf("[INFO] Kontena-OAuth2 Token %v: Read code=%v token=%v gone: %v", rd.Id(), code, token, forbiddenError)
-
-		rd.SetId("")
-
-	} else {
-		log.Printf("[INFO] Kontena-OAuth2 Token %v: Read code=%v token=%v err: %v", rd.Id(), code, token, err)
-
-		return err
-	}
-
-	return nil
+	return resourceKontenaTokenSync(providerMeta, rd)
 }
 
 func resourceKontenaTokenDelete(rd *schema.ResourceData, meta interface{}) error {
